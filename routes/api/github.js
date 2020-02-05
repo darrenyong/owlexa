47d5d016341644f47654916aaac21473333d8e5e
darrenyong <contact.darrenyong@gmail.com>
2020-02-04 16:21:58 -0800

    Extract Pull Request from JSON request

--- routes/api/github.js
+++ routes/api/github.js
@@ -6,4 +6,10 @@ router.get('/test', (req, res) => {
   res.json({ msg: 'This is working' })
 })
 
+router.post('/sendPR', (req, res) => {
+  const resPayload = JSON.parse(req.body.payload);
+  const pr = resPayload.pull_request;
+
+})
+
 module.exports = router;
