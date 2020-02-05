cb7c5f30a4aa9f6a87f791d35fc1f27e7a73a6fb
darrenyong <contact.darrenyong@gmail.com>
2020-02-04 16:25:26 -0800

    Add logic to check if PR is merged

--- routes/api/github.js
+++ routes/api/github.js
@@ -9,7 +9,10 @@ router.get('/test', (req, res) => {
 router.post('/sendPR', (req, res) => {
   const resPayload = JSON.parse(req.body.payload);
   const pr = resPayload.pull_request;
-
+  
+  // Check if it's merged
+  const prAction = resPayload.action;
+  const prMergeStatus = pr.merged;
 })
 
 module.exports = router;
